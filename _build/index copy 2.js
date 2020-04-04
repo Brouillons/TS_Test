b511d8ec030a971f4dbe0506a6e6a9985c68ca9e
Rick PYT <rick.info.rsoc@gmail.com>
2020-04-04 18:37:32 +0200

    rendu 26

--- _build/index copy 2.js
+++ _build/index copy 2.js
@@ -12,5 +12,5 @@ var test1 = function () {
         console.log("\nOn arrive de façon asynchrone dans le  then(...) !");
         console.log("pDataParam=" + pDataParam); //88
     });
-    console.log("FIN (exécutée avant le then).");
+    console.log("FIN (exécutée avant le then)."); // 545454
 };
