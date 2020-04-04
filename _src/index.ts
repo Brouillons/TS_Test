626cc065fa9ef395f3807cd3766bc2a9fb20c02c
Rick PYT <rick.info.rsoc@gmail.com>
2020-04-04 17:04:33 +0200

    First commit

---
+++ _src/index.ts
@@ -0,0 +1,20 @@
+let fMain=function() {
+    test1();
+};
+
+//---------------------------------------
+
+let test1=function() {
+  console.log("DEBUT - Avant Promise(...)");
+
+  new Promise((resolve, reject)=> {
+    console.log("  Dans  Promise(...) !");
+    resolve(88);
+  })
+  .then((pDataParam)=> {
+    console.log("\nOn arrive de façon asynchrone dans le  then(...) !");
+    console.log("pDataParam="+pDataParam);  //88
+  });
+
+  console.log("FIN (exécutée avant le then).");
+};
